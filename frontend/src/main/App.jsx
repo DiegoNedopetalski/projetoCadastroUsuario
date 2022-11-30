b6958ea029de2310b73e9a76b1975da8bd511a96
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-30 17:37:50 -0300

    add component UserCrud

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,18 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HashRouter } from 'react-router-dom';
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
 import Footer from '../components/templates/Footer'
+import Home from '../components/home/Home';
 
 export default function App() {
     return (
         <div className="app">
             <Logo />
             <Nav />
-            <Main icon="home" title="Início" subtitle="Segundo projeto do capítulo de React." />
+            <Home />
             <Footer />
         </div>
     )
